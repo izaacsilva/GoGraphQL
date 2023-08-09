b590507563be4436004b2d1a3134d1ba972d9652
Izaac <izcss21@gmail.com>
2023-08-09 01:01:15 +0000

    NewCategory isn't working.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/izaacsilva/GoGraphQL/graph"
+	"github.com/izaacsilva/GoGraphQL/internal/database"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -20,12 +21,18 @@ func main() {
 		log.Fatalf("failed to open database: %v", err)
 	}
 	defer db.Close()
+
+	categoryDb := database.NewCategoryDB(db)
+
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
